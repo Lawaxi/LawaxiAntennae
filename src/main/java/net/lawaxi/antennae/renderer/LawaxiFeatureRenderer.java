afb10359f6a5cefac14e9ed441dc30349d3566c2
Lawaxi <2330234142@qq.com>
2020-05-26 19:16:48 +0800

    Revert "支持1.14及以下"
    This reverts commit 32b82eacb5ee18485bb78ad27245f0d527d833e6.

---
+++ src/main/java/net/lawaxi/antennae/renderer/LawaxiFeatureRenderer.java
@@ -0,0 +1,41 @@
+package net.lawaxi.antennae.renderer;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+
+@Environment(EnvType.CLIENT)
+public class LawaxiFeatureRenderer extends FeatureRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
+
+    public LawaxiFeatureRenderer(FeatureRendererContext<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> featureRendererContext) {
+        super(featureRendererContext);
+    }
+
+    public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, AbstractClientPlayerEntity abstractClientPlayerEntity, float f, float g, float h, float j, float k, float l) {
+
+        if ("Lawaxi".equals(abstractClientPlayerEntity.getName()) && abstractClientPlayerEntity.hasSkinTexture() && !abstractClientPlayerEntity.isInvisible()) {
+
+            VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(RenderLayer.getEntitySolid(abstractClientPlayerEntity.getSkinTexture()));
+            int m = LivingEntityRenderer.getOverlay(abstractClientPlayerEntity, 0.0F);
+
+            matrixStack.push();
+
+
+
+
+            getContextModel().getHead().setTextureOffset(26, 0).addCuboid(-3.0F, -12.0F, -5.0F, 1.0F, 5.0F, 1.0F, 0.0F, false);
+            getContextModel().getHead().setTextureOffset(30, 0).addCuboid(2.0F, -12.0F, -5.0F, 1.0F, 5.0F, 1.0F, 0.0F, false);
+            getContextModel().getHead().render(matrixStack,vertexConsumer,i,m);
+            matrixStack.pop();
+
+        }
+    }
+}
